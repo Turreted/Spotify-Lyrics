4b1e34e38adc5d801bd5ee914dc1d67632ef2612
PappaStalin <41593269+PappaStalin@users.noreply.github.com>
2019-03-20 12:01:24 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -43,7 +43,7 @@ def song_data():
 
 
 headers_Get = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate',

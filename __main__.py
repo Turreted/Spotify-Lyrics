9cf752e71e4776e0b57bfc092f8e916104ac4291
PappaStalin <41593269+PappaStalin@users.noreply.github.com>
2019-03-21 15:28:03 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -8,6 +8,7 @@ import spotify_token as st
 import ast
 from datetime import datetime
 
+
 class Spotify:
     def __init__(self, user, passw):
         self.token = None
@@ -16,62 +17,65 @@ class Spotify:
         self.passw = passw
         self.gettoken()
         self.spotheaders = {'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.token}',
-            }
+                            'Content-Type': 'application/json',
+                            'Authorization': f'Bearer {self.token}',
+                            }
         self.lyricheaders = {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.5',
-                'Accept-Encoding': 'gzip, deflate',
-                'DNT': '1',
-                'Connection': 'keep-alive',
-                'Upgrade-Insecure-Requests': '1'
-            }
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1'
+        }
         self.artist = None
         self.song = None
         self.query = None
-        self.lyrics = None
+        self.lyrics = ''
+
     def gettoken(self):
         if self.token is None or self.token_exp < datetime.now():
             data = st.start_session(self.user, self.passw)
             self.token = data[0]
             self.token_exp = data[1]
+
     def getsong(self):
         response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=self.spotheaders)
         if response.status_code != 200:
             print(response.status_code)
-            print("Bad Response from Spotify API")    
+            print("Bad Response from Spotify API")
         else:
             json_data = json.loads(response.text)
             self.artist = json_data["item"]["artists"][0]["name"]
             if self.song != json_data["item"]["name"]:
                 self.song = json_data["item"]["name"]
-                self.query = f"{self.song}+{self.artist}+lyrics"
+                self.query = (str(self.song) + '+' + str(self.artist) + '+lyrics').replace(' ', '+')
                 self.getlyrics()
+
     def getlyrics(self):
         s = requests.Session()
-        url = f'https://www.google.com/search?q={self.query}&ie=utf-8&oe=utf-8'
+        url = 'https://www.google.com/search?q={}&ie=utf-8&oe=utf-8'.format(self.query)
         r = s.get(url, headers=self.lyricheaders)
         soup = BeautifulSoup(r.text, "html.parser").find_all("span", {"jsname": "YS01Ge"})
         for link in soup:
-            self.lyrics = f"{self.lyrics}\n{link.text}"
-        print(f"{self.song}' '{self.artist}\n")
+            self.lyrics += (link.text + '\n')
+        print(f"{self.song} by {self.artist}\n")
         print(f"{self.lyrics}\n")
 
 
 def main():
-    user = input("Please enter spotify username: ")
-    passw = input("Please enter spotify password: ")
+    user = input('Spotify username: ')
+    passw = input('Spotify password: ')
     spt = Spotify(user, passw)
     while True:
         try:
             spt.getsong()
             time.sleep(3)
         except KeyboardInterrupt:
-            quit() 
-    
-    
+            quit()
+
+
 if __name__ == "__main__":
     main()
-    
+

f242078f7f16e1f6733a3e27d46a9c9c7b6c4504
PappaStalin <41593269+PappaStalin@users.noreply.github.com>
2019-03-26 14:26:34 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -11,6 +11,8 @@ from collections import namedtuple
 import logging
 import argparse
 
+# checks for the existence of two environment variables to get the login credentials.
+# If none are found, it prompts the user for a username and password
 parser = argparse.ArgumentParser()
 parser.add_argument('--log', help='set the log level', choices=['INFO', 'DEBUG', 'ERROR'], default='ERROR')
 args = parser.parse_args()
@@ -38,10 +40,12 @@ class Spotify:
         self.user = user
         self.passw = passw
         self.gettoken()
+        # The headers for the GET request sent to the Spotify API in order to retrive the current song
         self.spotheaders = {'Accept': 'application/json',
                             'Content-Type': 'application/json',
                             'Authorization': f'Bearer {self.token}',
                             }
+        # The headers for the Google search used to find the song lyrics
         self.lyricheaders = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -57,6 +61,7 @@ class Spotify:
         self.lyrics = ''
 
     def gettoken(self):
+        # uses the spotify_token module to submit credentials to the spotify API and return a token and token experation date 
         logger.info('getting token')
         if self.token is None or self.token_exp < datetime.now():
             logger.info('no token found, starting new session')
@@ -68,6 +73,7 @@ class Spotify:
             logger.info(f'using cached token')
 
     def getsong(self):
+        # Submits a GET request to the Spotify API with the valid OAuth token which returns a JSON object containing the song + artist playing on the user's account  
         logger.debug('calling `getsong`')
         response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=self.spotheaders)
         logger.debug(f'status code: {response.status_code}')
@@ -81,20 +87,25 @@ class Spotify:
             raise e
         else:
             json_data = json.loads(response.text)
+            # parses the JSON object returned by the Spotify API and uses it to define self.song and self.artist
             self.artist = json_data["item"]["artists"][0]["name"]
             if self.song != json_data["item"]["name"]:
                 self.song = json_data["item"]["name"]
+                # Constructs a google query out of self.song and self.artist
                 self.query = (str(self.song) + '+' + str(self.artist) + '+lyrics').replace(' ', '+')
                 self.getlyrics()
 
     def getlyrics(self):
+        # makes a google search for the song playing on the user's account and extracts the song lyrics contained within the page
         logger.debug('calling `getlyrics`')
         self.lyrics = ''
         s = requests.Session()
         url = 'https://www.google.com/search?q={}&ie=utf-8&oe=utf-8'.format(self.query)
         logger.debug(f'url -> {url}')
+        # makes google search 
         r = s.get(url, headers=self.lyricheaders)
         logger.debug('response received... parsing')
+        # extracts the song lyrics from the Google page
         soup = BeautifulSoup(r.text, "html.parser").find_all("span", {"jsname": "YS01Ge"})
         for link in soup:
             self.lyrics += (link.text + '\n')
